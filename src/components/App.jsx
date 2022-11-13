3ed97d4e60dc95b8c6011e470539363e763899c9
Kerlyn Frias Hidalgo <khidalgo@viva.com.do>
2022-11-12 20:00:38 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return(
+        <div className="main-container">
+            <h1>Funciona</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import App from "./components/App";
+const app = ReactDOM.createRoot(document.getElementById("main-app"));
+
+app.render(
+ <App/>
+);
