95f59e694fac45f0ad2ad306ab6586f93bd677e9
Kerlyn Frias Hidalgo <khidalgo@viva.com.do>
2022-11-23 19:22:16 -0400

    fixing bad request on api

--- server/server.js
+++ server/server.js
@@ -7,26 +7,34 @@ app.use(cors());
 
 let answers = [];
 
-app.get('/answer', (m,res) => {
+app.get('/answer', (answer,res) => {
 
-    // const id = answer;
-    // console.log(id);
-    //answers.push(answer.body.repueta)
-    // if(answers.length === 2){
-    //     if(answers[0].arma === 0 && answers[1].arma === 2){
-    //         res.json("El jugador" + answers[0].id + "ha ganado")
-    //     }
-    //     else if(answers[0].arma === 1 && answers[1].arma === 0){
-    //         res.json("El jugador" + answers[0].id + "ha ganado")
-    //     }
-    //     else if(answers[0].arma === 2 && answers[1].arma === 1){
-    //         res.json("El jugador" + answers[0].id + "ha ganado")
-    //     }
-    //     else{
-    //         res.json("El jugador" + answers[1].id + "ha ganado")
-    //     }
-    // }
-    res.json({message : "ute e un rapasumadre"})
+    const id = answer.headers.hola;
+    console.log(id);
+    answers.push(id)
+    if(answers.length === 2){
+        if(answers[0].arma === 0 && answers[1].arma === 2){
+            res.json("El jugador" + answers[0].id + "ha ganado")
+            answers = []
+        }
+        else if(answers[0].arma === 1 && answers[1].arma === 0){
+            res.json("El jugador" + answers[0].id + "ha ganado")
+            answers = []
+        }
+        else if(answers[0].arma === 2 && answers[1].arma === 1){
+            res.json("El jugador" + answers[0].id + "ha ganado")
+            answers = []
+        }
+        else{
+            res.json("El jugador" + answers[1].id + "ha ganado")
+            answers = []
+        }
+    }
+    else{
+        
+        res.json({message : "Esperando la respuesta del contrincante"})
+        
+    }
 })
 
 
